4ba650b306ead1962260f84952c1aec3497ba212
Chris <chrislmc25@gmail.com>
2024-12-06 17:16:55 +0800

    initial commit

---
+++ src/main/java/com/chris-lai/pkm-tcg-pocket/App.java
@@ -0,0 +1,13 @@
+package com.chris_lai.pkm_tcg_pocket;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
