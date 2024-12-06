42c5e58a729d5291761c6288ba6475749c573132
Chris <chrislmc25@gmail.com>
2024-12-07 01:42:22 +0800

    formatter setting

--- src/main/java/com/chris_lai/pkm_tcg_pocket_helper/App.java
+++ src/main/java/com/chris_lai/pkm_tcg_pocket_helper/App.java
@@ -6,10 +6,8 @@ import com.chris_lai.pkm_tcg_pocket_helper.cardList.CardList;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+    public static void main(String[] args) {
         System.out.println("com.chris_lai.pkm_tcg_pocket_helper.App");
         CardList cardList = new CardList();
         cardList.log();
