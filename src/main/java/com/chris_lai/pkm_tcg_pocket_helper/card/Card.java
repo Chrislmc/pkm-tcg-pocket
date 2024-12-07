4759a6b6fdbaf9bd87af598a18f761c4304ab521
Chris <chrislmc25@gmail.com>
2024-12-07 18:52:29 +0800

    Card parseRarity method

--- src/main/java/com/chris_lai/pkm_tcg_pocket_helper/card/Card.java
+++ src/main/java/com/chris_lai/pkm_tcg_pocket_helper/card/Card.java
@@ -1,5 +1,8 @@
 package com.chris_lai.pkm_tcg_pocket_helper.card;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import com.chris_lai.pkm_tcg_pocket_helper.types.cardPack.CardPackEnum;
 import com.chris_lai.pkm_tcg_pocket_helper.types.rarity.RarityEnum;
 
@@ -8,7 +11,29 @@ public class Card {
     String name;
     CardPackEnum cardPack;
     RarityEnum rarity;
-    
+    HashMap<RarityEnum, String> rarityDesc = new HashMap<RarityEnum, String>(Map.ofEntries(
+            Map.entry(
+                    RarityEnum.NORMAL_ONE,
+                    "♢"),
+            Map.entry(
+                    RarityEnum.NORMAL_TWO,
+                    "♢♢"),
+            Map.entry(
+                    RarityEnum.NORMAL_THREE,
+                    "♢♢♢"),
+            Map.entry(
+                    RarityEnum.NORMAL_FOUR,
+                    "♢♢♢♢"),
+            Map.entry(
+                    RarityEnum.STAR_ONE,
+                    "☆"),
+            Map.entry(
+                    RarityEnum.STAR_TWO,
+                    "☆☆"),
+            Map.entry(
+                    RarityEnum.CROWN,
+                    "♛")));
+
     public Card(String cardId, String cardName, CardPackEnum cardPackName, RarityEnum cardRarity) {
         id = cardId;
         name = cardName;
@@ -16,7 +41,18 @@ public class Card {
         rarity = cardRarity;
     }
 
+    public String parseRarity() {
+        return rarityDesc.get(
+                rarity);
+    }
+
     public void log() {
-        System.out.println(String.format("This card is %2$s(%1$s) from the %3$s pack.", id, name, cardPack));
+        System.out.println(
+                String.format(
+                        "This %4$s card is %2$s(%1$s) from the %3$s pack.",
+                        id,
+                        name,
+                        cardPack,
+                        parseRarity()));
     }
 }
