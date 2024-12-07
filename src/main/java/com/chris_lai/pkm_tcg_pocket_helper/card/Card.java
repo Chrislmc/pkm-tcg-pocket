1f309304cbec7816cb3859a3523a7a9c7cb63a91
Chris <chrislmc25@gmail.com>
2024-12-07 15:21:43 +0800

    add rarityEnum to cardList

--- src/main/java/com/chris_lai/pkm_tcg_pocket_helper/card/Card.java
+++ src/main/java/com/chris_lai/pkm_tcg_pocket_helper/card/Card.java
@@ -1,16 +1,19 @@
 package com.chris_lai.pkm_tcg_pocket_helper.card;
 
 import com.chris_lai.pkm_tcg_pocket_helper.types.cardPack.CardPackEnum;
+import com.chris_lai.pkm_tcg_pocket_helper.types.rarity.RarityEnum;
 
 public class Card {
     String id;
     String name;
     CardPackEnum cardPack;
+    RarityEnum rarity;
     
-    public Card(String cardId, String cardName, CardPackEnum cardPackName) {
+    public Card(String cardId, String cardName, CardPackEnum cardPackName, RarityEnum cardRarity) {
         id = cardId;
         name = cardName;
         cardPack = cardPackName;
+        rarity = cardRarity;
     }
 
     public void log() {
--- src/main/java/com/chris_lai/pkm_tcg_pocket_helper/cardList/CardList.java
+++ src/main/java/com/chris_lai/pkm_tcg_pocket_helper/cardList/CardList.java
@@ -2,21 +2,23 @@ package com.chris_lai.pkm_tcg_pocket_helper.cardList;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+
 import com.chris_lai.pkm_tcg_pocket_helper.card.Card;
 import com.chris_lai.pkm_tcg_pocket_helper.types.cardPack.CardPackEnum;
+import com.chris_lai.pkm_tcg_pocket_helper.types.rarity.RarityEnum;
 
 public class CardList {
     ArrayList<Card> cardDataList = new ArrayList<Card>(Arrays.asList(
-            new Card("001", "Bulbasaur", CardPackEnum.MEWTWO),
-            new Card("002", "Ivysaur", CardPackEnum.MEWTWO),
-            new Card("003", "Venusaur", CardPackEnum.MEWTWO),
-            new Card("004", "Venusaur ex", CardPackEnum.MEWTWO),
-            new Card("005", "Caterpie", CardPackEnum.PIKACHU),
-            new Card("006", "Metapod", CardPackEnum.PIKACHU),
-            new Card("007", "Butterfree", CardPackEnum.PIKACHU),
-            new Card("008", "Weedle", CardPackEnum.MEWTWO),
-            new Card("009", "Kakuna", CardPackEnum.MEWTWO),
-            new Card("010", "Beedrill", CardPackEnum.MEWTWO)));
+            new Card("001", "Bulbasaur", CardPackEnum.MEWTWO, RarityEnum.NORMAL_ONE),
+            new Card("002", "Ivysaur", CardPackEnum.MEWTWO, RarityEnum.NORMAL_TWO),
+            new Card("003", "Venusaur", CardPackEnum.MEWTWO, RarityEnum.NORMAL_THREE),
+            new Card("004", "Venusaur ex", CardPackEnum.MEWTWO, RarityEnum.NORMAL_FOUR),
+            new Card("005", "Caterpie", CardPackEnum.PIKACHU, RarityEnum.NORMAL_ONE),
+            new Card("006", "Metapod", CardPackEnum.PIKACHU, RarityEnum.NORMAL_ONE),
+            new Card("007", "Butterfree", CardPackEnum.PIKACHU, RarityEnum.NORMAL_THREE),
+            new Card("008", "Weedle", CardPackEnum.MEWTWO, RarityEnum.NORMAL_ONE),
+            new Card("009", "Kakuna", CardPackEnum.MEWTWO, RarityEnum.NORMAL_ONE),
+            new Card("010", "Beedrill", CardPackEnum.MEWTWO, RarityEnum.NORMAL_THREE)));
 
     public void log() {
         for (Card card : cardDataList) {
